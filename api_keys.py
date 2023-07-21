6e1451fda111f90fceedb4aa63e21f291fc30347
Eleanor Hayden <135467024+eshayden0617@users.noreply.github.com>
2023-07-21 06:17:23 -0500

    setup part 2

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "1559278678906dc9f1d25c9d5fa92472"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "3c59bd7da9bb49c59a30d67ee667dac3"
